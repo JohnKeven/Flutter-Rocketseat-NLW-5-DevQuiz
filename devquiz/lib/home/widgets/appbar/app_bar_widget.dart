18a81adceade06e9045bac3b30540227a66717ec
John Keven <johnkeven.ti@gmail.com>
2021-04-20 02:27:23 -0300

    Aula 1
    Aula 1

---
+++ devquiz/lib/home/widgets/appbar/app_bar_widget.dart
@@ -0,0 +1,48 @@
+import 'package:devquiz/core/app_gradients.dart';
+import 'package:devquiz/core/app_text_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:devquiz/home/widgets/score_card/score_card_widget.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget()
+      : super(
+          preferredSize: Size.fromHeight(250),
+          child: Container(
+            height: 250,
+            child: Stack(
+              children: [
+                Container(
+                  height: 161,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(gradient: AppGradients.linear),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text.rich(TextSpan(
+                          text: "Olá, ",
+                          style: AppTextStyles.title,
+                          children: [
+                            TextSpan(
+                                text: "John", style: AppTextStyles.titleBold)
+                          ])),
+                      //Text("Olá, John", style: AppTextStyles.title ,),
+                      Container(
+                        width: 58,
+                        height: 59,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://avatars.githubusercontent.com/u/56613705?v=4")),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Align(alignment: Alignment(0.0, 1.0), child: ScoreCardWidget()),
+              ],
+            ),
+          ),
+        );
+}
